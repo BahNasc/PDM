5756ea9c2a5790881515026a0aca1c5704106cc7
Bárbara <barbara.n2005@aluno.ifsc.edu.br>
2023-08-16 12:02:14 -0300

    Meu primeiro app

---
+++ MeuPrimeiroApp/app/src/main/java/com/example/meuprimeiroapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.meuprimeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Ciclo de vida", "onCreated");
+        Toast.makeText(this, "onCreated", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Ciclo de vida", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Ciclo de vida", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Ciclo de vida", "onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
+    }
+}
