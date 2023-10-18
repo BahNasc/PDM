b335482e0785250c92beedcacabae19b8dc94a22
bah <barbara.n2005@aluno.ifsc.edu.br>
2023-10-18 11:46:40 -0300

    atualizado

--- database.teste2/databaseteste2/app/src/main/java/com/example/databaseteste2/MainActivity.java
+++ database.teste2/databaseteste2/app/src/main/java/com/example/databaseteste2/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.databaseteste2;
 
+import static com.example.databaseteste2.R.id.listView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    EditText editText;
+    SQLiteDatabase sqlLiteDatabase;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.listView);
+        editText=findViewById(R.id.edText);
+        sqlLiteDatabase=openOrCreateDatabase("notas", MODE_PRIVATE, null);
+        sqlLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIARY KEY AUTOINCREMENT, txt varchar)");
+    }
+    public void inserirNot(){
+
     }
 }
