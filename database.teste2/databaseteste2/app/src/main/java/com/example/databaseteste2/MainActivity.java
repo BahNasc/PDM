baa7429d12aa9cb5a30630c7efb96ab4e771a2ac
Bábara <barbara.n2005@aluno.ifsc.edu.br>
2023-10-25 10:58:54 -0300

    Database finalizado

--- database.teste2/databaseteste2/app/src/main/java/com/example/databaseteste2/MainActivity.java
+++ database.teste2/databaseteste2/app/src/main/java/com/example/databaseteste2/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.databaseteste2;
-
 import static com.example.databaseteste2.R.id.listView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -24,8 +21,30 @@ public class MainActivity extends AppCompatActivity {
         editText=findViewById(R.id.edText);
         sqlLiteDatabase=openOrCreateDatabase("notas", MODE_PRIVATE, null);
         sqlLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIARY KEY AUTOINCREMENT, txt varchar)");
-    }
-    public void inserirNot(){
 
+        ContentValues cv = new ContentValues();
+        cv.put("nome", "Bah");
+        sqlitedb.insert("usr", null, cv);
+
+        Cursor cursor = sqlitedb.rawQuery("SELECT * FROM usr", null);
+        cursor.moveToFirst();
+
+        ArrayList<String> listaUsuario = new ArrayList<>();
+
+        while(!cursor.isAfterLast()) {
+            listaUsuario.add(cursor.getString(cursor.getColumnIndex("nome")));
+            Log.d("tableusr", cursor.getString(cursor.getColumnIndex("id"))
+                    + cursor.getString(cursor.getColumnIndex("nome")));
+            cursor.moveToNext();
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                listaUsuario
+        );
+        listaUsuarios.setAdapter(arrayAdapter);
     }
 }
+
+
