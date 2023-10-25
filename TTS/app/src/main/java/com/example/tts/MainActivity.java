8c9b6fefa4ed905dd9d025268947ff77cf6f716e
Bábara <barbara.n2005@aluno.ifsc.edu.br>
2023-10-25 11:22:03 -0300

    TTS finalizado

--- TTS/app/src/main/java/com/example/tts/MainActivity.java
+++ TTS/app/src/main/java/com/example/tts/MainActivity.java
@@ -2,30 +2,38 @@ package com.example.tts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-
+import android.view.View;
+import android.widget.EditText;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     private TextToSpeech tts;
+    private boolean ttsLoaded = false;
+    private EditText edFala;
+    String fala;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tts = new TextToSpeech(this, this);
+        edFala = findViewById(R.id.edFala);
     }
-
+    @Override
     public void onInit(int status) {
         if (status == TextToSpeech.SUCCESS) {
             tts.setLanguage(Locale.getDefault());
-
+            ttsLoaded = true;
         }
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
+    public void falar(View view) {
+        fala = edFala.getText().toString();
+        if(ttsLoaded) {
+            tts.speak(fala, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
     }
 }
