847683c9cdf8740bb03a318c82b429fa8f3f582e
BahNasc <barbara.n2005@aluno.ifsc.edu.br>
2023-11-01 11:27:31 -0300

    Laucher

---
+++ Laucher/app/src/main/java/com/example/laucher/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.laucher;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview_apps);
+        PackageManager packageManager = getPackageManager();
+        List<ApplicationInfo> packageInfoList = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> appList = packageManager.queryIntentActivities(mainIntent, 0);
+
+        AppAdapter appAdapter = new AppAdapter(this,R.layout.itens, packageInfoList);
+        listView.setAdapter(appAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ApplicationInfo applicationInfo = (ApplicationInfo) parent.getItemAtPosition(position);
+                if (applicationInfo != null) {
+                    startApp(applicationInfo.packageName);
+                }
+            }
+        });
+
+
+    }
+
+    private void startApp(String packageName) {
+        try {
+            Intent launchIntent = getPackageManager().getLaunchIntentForPackage(packageName);
+            if (launchIntent != null) {
+                startActivity(launchIntent);
+            } else {
+                Toast.makeText(this, "Aplicativo não encontrado", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Erro ao iniciar o aplicativo", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
